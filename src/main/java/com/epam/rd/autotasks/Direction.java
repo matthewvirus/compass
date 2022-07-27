06c5c8d5c714ba7484e44409cc057b730716dbf7
Matthew Zhigalo <matthewzhigalo@gmail.com>
2022-07-27 18:28:37 +0300

    solved

--- src/main/java/com/epam/rd/autotasks/Direction.java
+++ src/main/java/com/epam/rd/autotasks/Direction.java
@@ -1,7 +1,5 @@
 package com.epam.rd.autotasks;
 
-import java.util.Optional;
-
 public enum Direction {
     N(0), NE(45), E(90), SE(135), S(180), SW(225), W(270), NW(315);
 
@@ -9,21 +7,59 @@ public enum Direction {
         this.degrees = degrees;
     }
 
-    private int degrees;
+    private final int degrees;
+
+    private static int toRange(int degrees) {
+        while (degrees < 0) degrees += 360;
+        return degrees % 360;
+    }
 
     public static Direction ofDegrees(int degrees) {
-        throw new UnsupportedOperationException();
+        degrees = toRange(degrees);
+        for (Direction dir: Direction.values()) {
+            if (dir.degrees == degrees)
+                return dir;
+        }
+        return null;
     }
 
     public static Direction closestToDegrees(int degrees) {
-        throw new UnsupportedOperationException();
+        degrees = toRange(degrees);
+        if (degrees < 25) return N;
+        else if (degrees < 70) return NE;
+        else if (degrees < 115) return E;
+        else if (degrees < 150) return SE;
+        else if (degrees < 205) return S;
+        else if (degrees < 250) return SW;
+        else if (degrees < 295) return W;
+        else if (degrees < 340) return NW;
+        return N;
     }
 
     public Direction opposite() {
-        throw new UnsupportedOperationException();
+        switch (this) {
+            case N:
+                return S;
+            case NE:
+                return SW;
+            case E:
+                return W;
+            case SE:
+                return NW;
+            case S:
+                return N;
+            case SW:
+                return NE;
+            case W:
+                return E;
+            case NW:
+                return SE;
+        }
+        return null;
     }
 
     public int differenceDegreesTo(Direction direction) {
-        throw new UnsupportedOperationException();
+        int difference = Math.abs(this.degrees - direction.degrees);
+        return Math.min(difference, 360 - difference);
     }
 }
